983b65200c82e84f2b4e14ae3d19adbd4250d371
HusnainAnwar <webdeveloping@gmail.com>
2024-07-08 13:41:36 +0500

    New

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        The lists of tasks
+    </h1>
+
+    <div>
+@forelse ($tasks as $task)
+
+    <div> {{$task->title}}</div>
+@empty
+    <div> There are no tasks !</div>
+    
+@endforelse
+    </div>
+</body>
+</html>
